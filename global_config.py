27654e35d25b23c2cbe8b752a43569270bc700e2
Mykael A. <mykaelalvesnogueira1@gmail.com>
2025-01-28 21:38:10 -0300

    detect os for sql driver

--- global_config.py
+++ global_config.py
@@ -1,4 +1,11 @@
-db_config = {'Driver': '{SQL Server}',
+db_config_windows = {'Driver': '{SQL Server}',
+            'Server':'34.151.220.250;',
+            'Database':'testando1',
+            'Trusted_Connection':'no;',
+            'uid':'sqlserver;',
+            'pwd':'proarc;'}
+
+db_config_linux = {'Driver': '{/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.6.1}',
             'Server':'34.151.220.250;',
             'Database':'testando1',
             'Trusted_Connection':'no;',
@@ -6,7 +13,12 @@ db_config = {'Driver': '{SQL Server}',
             'pwd':'proarc;'}
 
 def get_db_config():
-    return db_config
+    from sys import platform
+
+    if platform == "win32":
+        return db_config_windows
+    else:
+        return db_config_linux
 
 #VM: /opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.6.1
 #Local: SQL Server
