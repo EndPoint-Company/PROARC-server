dbfd5c159a4f5dbed43c1ebfaf0940f283d73a14
Mykael A. <mykaelalvesnogueira1@gmail.com>
2025-02-04 18:50:26 -0300

    update tables

--- create_tables.sql
+++ create_tables.sql
@@ -3,19 +3,16 @@ CREATE TABLE Reclamados(
     nome NVARCHAR(150) NOT NULL,
     cpf NCHAR(11) NULL,
     cnpj NCHAR(14) NULL,
-    numero_rua SMALLINT NULL,
-    email NVARCHAR(150) NULL CONSTRAINT chk_email CHECK (email like '%_@__%.__%'),
-    rua NVARCHAR(110) NULL,
-    bairro NVARCHAR(100) NULL,
-    cidade NVARCHAR(100) NULL,
-    uf NCHAR(2) NULL,
+    numero_rua SMALLINT NOT NULL,
+    email NCHAR(150) NULL CHECK (email IS NULL OR email LIKE '%_@__%.__%'),
+    cep NCHAR(8) NOT NULL,
     created_at DATETIME DEFAULT GETDATE()
 );
 
 CREATE TABLE Usuarios(
     usuario_id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
-    nome NVARCHAR(50) NOT NULL,
-    nivel_permissao SMALLINT NOT NULL CONSTRAINT digito_unico CHECK (nivel_permissao BETWEEN 0 and 4),
+    nome NVARCHAR(100) NOT NULL,
+    cargo NVARCHAR(30) NOT NULL,
     hash_and_salt NVARCHAR(100) NOT NULL,
     salt NVARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT GETDATE()
@@ -30,29 +27,29 @@ CREATE TABLE Motivos(
 CREATE TABLE Reclamantes(
     reclamante_id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
     nome NVARCHAR(100) NOT NULL,
-    rg NVARCHAR(20) NOT NULL,
-    cpf NCHAR(11) NULL,
+    rg NVARCHAR(10) NULL,
+    cpf NCHAR(11) NOT NULL,
     created_at DATETIME DEFAULT GETDATE(),
 );
 
 CREATE TABLE Procuradores(
     procurador_id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
     nome NVARCHAR(100) NOT NULL,
-    rg NVARCHAR(20) NOT NULL,
-    cpf NCHAR(11) NULL,
+    rg NVARCHAR(10) NULL,
+    cpf NCHAR(11) NOT NULL,
     created_at DATETIME DEFAULT GETDATE(),
 );
 
-CREATE TABLE ProcessosAdministrativos(
-    processo_id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
-    motivo_id INT NULL,
-    reclamante_id INT NULL,
+CREATE TABLE Reclamacoes(
+    reclamacao_id INT PRIMARY KEY NOT NULL IDENTITY(1, 1),
+    motivo_id INT NOT NULL,
+    reclamante_id INT NOT NULL,
     procurador_id INT NULL,
-    titulo_processo NVARCHAR(10) NOT NULL UNIQUE,
-    status_processo NVARCHAR(50) NULL,
-    path_processo NVARCHAR(200) NOT NULL UNIQUE,
-    ano SMALLINT NOT NULL,
-    data_audiencia DATETIME NULL,
+    titulo NVARCHAR(10) NOT NULL UNIQUE,
+    situacao NVARCHAR(75) NOT NULL,
+    caminho_dir NVARCHAR(200) NOT NULL UNIQUE,
+    data_abertura DATE NOT NULL,
+    ultima_mudanca DATETIME DEFAULT GETDATE(),
     created_at DATETIME DEFAULT GETDATE(),
 
     FOREIGN KEY (reclamante_id) REFERENCES Reclamantes(reclamante_id)
@@ -66,12 +63,34 @@ CREATE TABLE ProcessosAdministrativos(
         ON UPDATE CASCADE
 );
 
+CREATE TABLE ReclamacoesGeral(
+    reclamacao_id INT NOT NULL,
+    data_audiencia DATETIME NULL,
+    conciliadora NVARCHAR(100) NULL,
+
+    FOREIGN KEY (reclamacao_id) REFERENCES Reclamacoes(reclamacao_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,    
+);
+
+CREATE TABLE ReclamacoesEnel(
+    reclamacao_id INT NOT NULL,
+    atendente NVARCHAR(100) NULL,
+    contato_enel_telefone NCHAR(11) NULL,
+    contato_enel_email NCHAR(150) NULL CHECK (contato_enel_email IS NULL OR contato_enel_email LIKE '%_@__%.__%'),
+    observacao TEXT NULL,
+
+    FOREIGN KEY (reclamacao_id) REFERENCES Reclamacoes(reclamacao_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,    
+);
+
 CREATE TABLE RelacaoProcessoReclamado(
-    processo_id INT NOT NULL,
+    reclamacao_id INT NOT NULL,
     reclamado_id INT NULL,
     created_at DATETIME DEFAULT GETDATE(),
 
-    FOREIGN KEY (processo_id) REFERENCES ProcessosAdministrativos(processo_id)
+    FOREIGN KEY (reclamacao_id) REFERENCES Reclamacoes(reclamacao_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
     FOREIGN KEY (reclamado_id) REFERENCES Reclamados(reclamado_id)
@@ -80,12 +99,12 @@ CREATE TABLE RelacaoProcessoReclamado(
 );
 
 CREATE TABLE HistoricoMudancaStatus(
-    processo_id INT,
+    reclamacao_id INT,
     status_old NVARCHAR(50) NULL,
     status_new NVARCHAR(50) NOT NULL,
-    created_at DATETIME DEFAULT GETDATE(),
+    changed_at DATETIME DEFAULT GETDATE(),
 
-    FOREIGN KEY (processo_id) REFERENCES ProcessosAdministrativos(processo_id)
+    FOREIGN KEY (reclamacao_id) REFERENCES Reclamacoes(reclamacao_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
 );
